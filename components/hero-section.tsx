f3171a7f10a3bd84b439dd8ba5eb66c3e71897d9
Dorukkozkn <77771219+Dorukkozkn@users.noreply.github.com>
2025-09-18 21:20:37 +0300

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -16,13 +16,13 @@ export function HeroSection() {
               <div className="text-white text-center px-4 max-w-4xl mx-auto">
                 <h1 className="text-4xl md:text-6xl font-bold mb-4 text-balance">
                 
-                  <span className="block text-destructive">TEDD MUN</span>
+                  <span className="block text-destructive">TEDDMUN</span>
                 </h1>
                 <p className="text-xl md:text-2xl mb-8 text-pretty max-w-3xl mx-auto">
                   Join us on a journey of diplomacy, leadership and global awareness
                 </p>
                 <Button size="lg" className="hover:bg-accent/90 text-accent-foreground bg-destructive">
-                  Apply to Club
+                  Apply Now
                   <ArrowRight className="ml-2 h-5 w-5" />
                 </Button>
               </div>
